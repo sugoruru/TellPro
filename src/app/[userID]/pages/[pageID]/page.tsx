806566683a01aafb6cd969f2f1cde1417cede774
ruku <hamaro1818@gmail.com>
2024-04-18 01:07:27 +0900

    非公開のページの分岐を追加

--- src/app/[userID]/pages/[pageID]/page.tsx
+++ src/app/[userID]/pages/[pageID]/page.tsx
@@ -85,50 +85,67 @@ export default function Page({ params }: { params: { userID: string; pageID: str
       <Loading />
     </>
   ) : isExist ? (
-    <>
-      <div className="text-center text-4xl font-bold text-gray-700 my-5">{page.title === "" ? "untitled" : page.title}</div>
-      <div className="text-center text-base font-bold text-gray-700">公開日時:{page.date.split("T")[0]}</div>
-      <div className="mx-auto">
-        <div className="flex mt-2 px-1 flex-wrap">
-          {page.tags.map((e) => (
-            <div className="select-none m-2 px-2 cursor-pointer flex rounded-sm h-6 bg-slate-300" key={returnRandomString(32)}>
-              <FaTag className="inline-flex my-auto mr-1" />
-              {tagJSON.tags[Number(e)].name}
-            </div>
-          ))}
+    myID === params.userID || page.isPublic ? (
+      <>
+        <div className="text-center text-4xl font-bold text-gray-700 my-5">{page.title === "" ? "untitled" : page.title}</div>
+        <div className="text-center text-base font-bold text-gray-700">公開日時:{page.date.split("T")[0]}</div>
+        <div className="mx-auto">
+          <div className="flex mt-2 px-1 flex-wrap">
+            {page.tags.map((e) => (
+              <div className="select-none m-2 px-2 cursor-pointer flex rounded-sm h-6 bg-slate-300" key={returnRandomString(32)}>
+                <FaTag className="inline-flex my-auto mr-1" />
+                {tagJSON.tags[Number(e)].name}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="mx-auto text-base font-bold text-gray-700">
-        <Link href={`/${params.userID}`} className="flex cursor-pointer">
-          <Image src={userIcon} alt="" width={24} height={24} className="mr-1" priority />
-          <u>@{params.userID}</u>
+        <div className="mx-auto text-base font-bold text-gray-700">
+          <Link href={`/${params.userID}`} className="flex cursor-pointer">
+            <Image src={userIcon} alt="" width={24} height={24} className="mr-1" priority />
+            <u>@{params.userID}</u>
+          </Link>
+        </div>
+        <div className="lg:w-3/5 w-full bg-white mx-auto my-3 p-5">{content}</div>
+        <Link title="編集" className={`cursor-pointer ${myID === params.userID ? "" : "hidden"}`} href={`/${params.userID}/pages/${params.pageID}/edit`}>
+          <div className="flex items-center justify-center w-16 h-16 bg-slate-300 hover:bg-blue-200 transition rounded-full fixed bottom-[30px] right-[30px]">
+            <MdEditNote className="inline-flex text-4xl" />
+          </div>
         </Link>
-      </div>
-      <div className="lg:w-3/5 w-full bg-white mx-auto my-3 p-5">{content}</div>
-      <Link title="編集" className={`cursor-pointer ${myID === params.userID ? "" : "hidden"}`} href={`/${params.userID}/pages/${params.pageID}/edit`}>
-        <div className="flex items-center justify-center w-16 h-16 bg-slate-300 hover:bg-blue-200 transition rounded-full fixed bottom-[30px] right-[30px]">
-          <MdEditNote className="inline-flex text-4xl" />
+        <div
+          className={`cursor-pointer flex items-center justify-center w-16 h-16 bg-slate-300 hover:bg-blue-200 transition rounded-full fixed ${
+            myID === params.userID ? "bottom-[120px]" : "bottom-[30px]"
+          } right-[30px]`}
+          title="いいね"
+          onClick={handleGoodButton}
+        >
+          {isLike ? <FaHeart className="inline-flex text-3xl text-red-500" /> : <FaRegHeart className="inline-flex text-3xl text-red-500" />}
         </div>
-      </Link>
-      <div
-        className={`cursor-pointer flex items-center justify-center w-16 h-16 bg-slate-300 hover:bg-blue-200 transition rounded-full fixed ${
-          myID === params.userID ? "bottom-[120px]" : "bottom-[30px]"
-        } right-[30px]`}
-        title="いいね"
-        onClick={handleGoodButton}
-      >
-        {isLike ? <FaHeart className="inline-flex text-3xl text-red-500" /> : <FaRegHeart className="inline-flex text-3xl text-red-500" />}
+        <div
+          className={`cursor-pointer flex items-center justify-center w-16 h-16 bg-slate-300 hover:bg-blue-200 transition rounded-full fixed ${
+            myID === params.userID ? "bottom-[210px]" : "bottom-[120px]"
+          } right-[30px]`}
+          title="ブックマーク"
+          onClick={handleBookmark}
+        >
+          {isBookmark ? <FaBookmark className="inline-flex text-3xl text-blue-500" /> : <FaRegBookmark className="inline-flex text-3xl text-blue-500" />}
+        </div>
+      </>
+    ) : (
+      // ページが非公開の時.
+      <div className="h-full bg-slate-100 text-center text-2xl font-black text-gray-600 py-10">
+        <div className="flex justify-center">
+          <BsExclamationCircle className="text-green-500 text-6xl" />
+        </div>
+        <p>ページが非公開です</p>
+        <p className="text-sm pt-5">
+          <span>(</span>
+          <Link href="/" className="text-blue-300">
+            こちら
+          </Link>
+          <span>からホームに戻ることが出来ます)</span>
+        </p>
       </div>
-      <div
-        className={`cursor-pointer flex items-center justify-center w-16 h-16 bg-slate-300 hover:bg-blue-200 transition rounded-full fixed ${
-          myID === params.userID ? "bottom-[210px]" : "bottom-[120px]"
-        } right-[30px]`}
-        title="ブックマーク"
-        onClick={handleBookmark}
-      >
-        {isBookmark ? <FaBookmark className="inline-flex text-3xl text-blue-500" /> : <FaRegBookmark className="inline-flex text-3xl text-blue-500" />}
-      </div>
-    </>
+    )
   ) : (
     // ページが存在しない時.
     <div className="h-full bg-slate-100 text-center text-2xl font-black text-gray-600 py-10">
