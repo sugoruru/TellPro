353e91d81446b0baf1c99b74fa3f63ce64b155b2
ruku <hamaro1818@gmail.com>
2024-04-18 20:34:04 +0900

    ホームのナビゲーションバーのprefetch属性の削除

--- src/app/components/navigationBar.tsx
+++ src/app/components/navigationBar.tsx
@@ -9,19 +9,13 @@ const NavigationBar = (props: { pathName: string }) => {
     <nav className="bg-white">
       <ul className="flex text-base mx-auto max-w-screen-2xl px-2 md:px-8">
         <li className={"px-2 font-medium" + " " + IsLocation("/")}>
-          <Link href="/" prefetch>
-            Home
-          </Link>
+          <Link href="/">Home</Link>
         </li>
         <li className={"px-2 font-medium" + " " + IsLocation("/questions")}>
-          <Link href="/questions" prefetch>
-            Questions
-          </Link>
+          <Link href="/questions">Questions</Link>
         </li>
         <li className={"px-2 font-medium" + " " + IsLocation("/pages")}>
-          <Link href="/pages" prefetch>
-            Pages
-          </Link>
+          <Link href="/pages">Pages</Link>
         </li>
       </ul>
     </nav>
