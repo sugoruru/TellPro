71122d443110c4458117a39cb7590bf445471be8
ruku <hamaro1818@gmail.com>
2024-06-25 02:08:01 +0900

    コンパイルエラーの修正

--- src/app/termsOfService/page.tsx
+++ src/app/termsOfService/page.tsx
@@ -1,23 +1,4 @@
-import { Metadata } from "next";
 import TermsOfService from "./client_page";
-
-export const metadata: Metadata = {
-  openGraph: {
-    url: process.env.NEXT_PUBLIC_TRUTH_URL,
-    title: `TellPro`,
-    siteName: "TellPro",
-    type: "article",
-    description: "TellProのホームページです。",
-    images: {
-      url: `${process.env.NEXT_PUBLIC_TRUTH_URL}/api/og`,
-      width: 1200,
-      height: 630,
-    },
-  },
-  twitter: {
-    card: "summary_large_image",
-  },
-};
 
 export default function Page() {
   return (
