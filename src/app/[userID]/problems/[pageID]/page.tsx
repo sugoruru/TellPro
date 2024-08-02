6b0fec9a87f9de5e36196c85a53e3fd8776c8ba9
ruku <hamaro1818@gmail.com>
2024-08-03 01:41:59 +0900

    公開後に発見されたバグの修正2

--- src/app/[userID]/problems/[pageID]/page.tsx
+++ src/app/[userID]/problems/[pageID]/page.tsx
@@ -2,15 +2,15 @@ import { Metadata } from "next";
 import Problems from "./client_page";
 import axios from "axios";
 
-export const generateMetadata = async ({ params }: { params: { userID: string } }): Promise<Metadata> => {
-  const user = (await axios.get(`${process.env.NEXT_PUBLIC_TRUTH_URL}/api/pages/user_meta?userID=${params.userID}`)).data;
+export const generateMetadata = async ({ params }: { params: { userID: string; pageID: string } }): Promise<Metadata> => {
+  const user = (await axios.get(`${process.env.NEXT_PUBLIC_TRUTH_URL}/api/pages/user_meta?pageID=${params.pageID}&pageType=problems&userID=${params.userID}`)).data;
   return {
     openGraph: {
       url: process.env.NEXT_PUBLIC_TRUTH_URL,
       title: `@${params.userID}｜TellPro`,
       siteName: "TellPro",
       type: "article",
-      description: "TellProの問題ページです。",
+      description: "TellProの問題集ページです。",
       images: {
         url: user.ok
           ? `${process.env.NEXT_PUBLIC_TRUTH_URL}/api/og?OGPType=user&userID=${params.userID}&userStatusMessage=${user.user.status_message}&userIcon=${user.user.icon}`
