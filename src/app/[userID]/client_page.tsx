5705a4b1b41b475f151b049e83e6aaebe1402dba
ruku <hamaro1818@gmail.com>
2024-08-14 11:08:24 +0900

    精進ツリーの作成中マークを導入

--- src/app/[userID]/client_page.tsx
+++ src/app/[userID]/client_page.tsx
@@ -176,8 +176,8 @@ export default function UserPage({ params }: { params: { userID: string } }) {
   return isLoading ? (
     <div className="h-full"></div>
   ) : isExist ? (
-    <div className="h-full">
-      <div className="mb-2">
+    <div className="h-full flex flex-col">
+      <div>
         <div className={`${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-white text-black"}`}>
           <div className="p-5 md:flex sm:block">
             <img alt={pageUser.username} src={pageUser.icon} width={100} height={100} className="md:mx-5 w-24 h-24" />
@@ -348,113 +348,116 @@ export default function UserPage({ params }: { params: { userID: string } }) {
             </div>
           </Dialog>
         </Transition>
-        <div className={`${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-slate-100 text-black"}`}>
-          {navPlace === "articles" ? (
-            pages.length === 0 ? (
-              <div className="m-5">記事はありません</div>
-            ) : (
-              <div className={`${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-slate-100 text-black"}`}>
-                {pages.map((page) => (
-                  <div key={returnRandomString(32)}>
-                    <PageLinkBlock page={page} pageUser={pageUser} pageType="articles" me={me} stateFunctions={{ setIsOpenDeletePageModal, setDeletePageID }} />
-                  </div>
-                ))}
-              </div>
-            )
-          ) : navPlace === "questions" ? (
-            questions.length === 0 ? (
-              <div className="m-5">質問はありません</div>
-            ) : (
-              <div className={`${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-slate-100 text-black"}`}>
-                {questions.map((question) => (
-                  <div key={returnRandomString(32)}>
-                    <PageLinkBlock page={question} pageUser={pageUser} pageType="questions" me={me} stateFunctions={{ setIsOpenDeletePageModal, setDeletePageID }} />
-                  </div>
-                ))}
-              </div>
-            )
-          ) : navPlace === "problems" ? (
-            problems.length === 0 ? (
-              <div className="m-5">問題集はありません</div>
-            ) : (
-              <div className={`${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-slate-100 text-black"}`}>
-                {problems.map((problem) => (
-                  <div key={returnRandomString(32)}>
-                    <PageLinkBlock page={problem} pageUser={pageUser} pageType="problems" me={me} stateFunctions={{ setIsOpenDeletePageModal, setDeletePageID }} />
-                  </div>
-                ))}
-              </div>
-            )
-          ) : (
-            <div className="m-5">My精進ツリーはありません</div>
-          )}
-          <Transition appear show={isOpenDeletePageModal} as={Fragment}>
-            <Dialog as="div" className="relative z-10" onClose={() => setIsOpenDeletePageModal(false)}>
-              <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-                <div className="fixed inset-0 bg-black/25" />
-              </Transition.Child>
-              <div className="fixed inset-0 overflow-y-auto">
-                <div className="flex min-h-full items-center justify-center p-4 text-center">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                  >
-                    <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                      <Dialog.Title as="h3" className="text-lg text-center font-medium leading-6 text-gray-900">
-                        {isDeleteSending ? "削除中です..." : "本当に削除しますか？"}
-                      </Dialog.Title>
-                      {isDeleteSending ? (
-                        <>
-                          <div className="flex justify-center mt-2" aria-label="読み込み中">
-                            <div className="animate-ping h-4 w-4 bg-blue-600 rounded-full"></div>
-                          </div>
-                        </>
-                      ) : (
-                        <>
-                          <div className="mt-2 text-center">
-                            <p className="text-sm text-gray-500">この操作はもとに戻せません</p>
-                          </div>
-                        </>
-                      )}
-                      <div className="mt-4 flex justify-center">
-                        <button
-                          type="button"
-                          className="mr-2 inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
-                          onClick={() => {
-                            if (navPlace === "articles") {
-                              deletePage("articles");
-                            } else if (navPlace === "questions") {
-                              deletePage("questions");
-                            } else if (navPlace === "problems") {
-                              deletePage("problems");
-                            }
-                          }}
-                          disabled={isDeleteSending}
-                        >
-                          <b>削除する</b>
-                        </button>
-                        <button
-                          type="button"
-                          className="mx-2 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                          onClick={() => setIsOpenDeletePageModal(false)}
-                          disabled={isDeleteSending}
-                        >
-                          <b>キャンセル</b>
-                        </button>
-                      </div>
-                    </Dialog.Panel>
-                  </Transition.Child>
-                </div>
-              </div>
-            </Dialog>
-          </Transition>
-        </div>
       </div>
+      <div className={`h-full ${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-slate-100 text-black"}`}>
+        {navPlace === "articles" ? (
+          pages.length === 0 ? (
+            <div className="m-5">記事はありません</div>
+          ) : (
+            <div className={`${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-slate-100 text-black"}`}>
+              {pages.map((page) => (
+                <div key={returnRandomString(32)}>
+                  <PageLinkBlock page={page} pageUser={pageUser} pageType="articles" me={me} stateFunctions={{ setIsOpenDeletePageModal, setDeletePageID }} />
+                </div>
+              ))}
+            </div>
+          )
+        ) : navPlace === "questions" ? (
+          questions.length === 0 ? (
+            <div className="m-5">質問はありません</div>
+          ) : (
+            <div className={`${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-slate-100 text-black"}`}>
+              {questions.map((question) => (
+                <div key={returnRandomString(32)}>
+                  <PageLinkBlock page={question} pageUser={pageUser} pageType="questions" me={me} stateFunctions={{ setIsOpenDeletePageModal, setDeletePageID }} />
+                </div>
+              ))}
+            </div>
+          )
+        ) : navPlace === "problems" ? (
+          problems.length === 0 ? (
+            <div className="m-5">問題集はありません</div>
+          ) : (
+            <div className={`${headerData.user.isDarkMode ? "bg-neutral-800 text-white" : "bg-slate-100 text-black"}`}>
+              {problems.map((problem) => (
+                <div key={returnRandomString(32)}>
+                  <PageLinkBlock page={problem} pageUser={pageUser} pageType="problems" me={me} stateFunctions={{ setIsOpenDeletePageModal, setDeletePageID }} />
+                </div>
+              ))}
+            </div>
+          )
+        ) : (
+          <div>
+            <p>機能作成中...🏗️</p>
+            <p>少しお待ち下さい</p>
+          </div>
+        )}
+      </div>
+      <Transition appear show={isOpenDeletePageModal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={() => setIsOpenDeletePageModal(false)}>
+          <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title as="h3" className="text-lg text-center font-medium leading-6 text-gray-900">
+                    {isDeleteSending ? "削除中です..." : "本当に削除しますか？"}
+                  </Dialog.Title>
+                  {isDeleteSending ? (
+                    <>
+                      <div className="flex justify-center mt-2" aria-label="読み込み中">
+                        <div className="animate-ping h-4 w-4 bg-blue-600 rounded-full"></div>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="mt-2 text-center">
+                        <p className="text-sm text-gray-500">この操作はもとに戻せません</p>
+                      </div>
+                    </>
+                  )}
+                  <div className="mt-4 flex justify-center">
+                    <button
+                      type="button"
+                      className="mr-2 inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                      onClick={() => {
+                        if (navPlace === "articles") {
+                          deletePage("articles");
+                        } else if (navPlace === "questions") {
+                          deletePage("questions");
+                        } else if (navPlace === "problems") {
+                          deletePage("problems");
+                        }
+                      }}
+                      disabled={isDeleteSending}
+                    >
+                      <b>削除する</b>
+                    </button>
+                    <button
+                      type="button"
+                      className="mx-2 inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={() => setIsOpenDeletePageModal(false)}
+                      disabled={isDeleteSending}
+                    >
+                      <b>キャンセル</b>
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   ) : (
     <div className="h-full">
