2c1816906f98bcf32faa77fd9c35b81a60ef95fd
anmoti <77442643+anmoti@users.noreply.github.com>
2024-08-17 20:53:38 +0900

    chore: 規約同意のテキストが見えなかったので修正

--- src/app/init/client_page.tsx
+++ src/app/init/client_page.tsx
@@ -352,7 +352,7 @@ export default function Init() {
                   value=""
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
-                <label htmlFor="termsOfService-checkbox" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                <label htmlFor="termsOfService-checkbox" className="ms-2 text-sm font-medium text-gray-900">
                   利用規約・プライバシーポリシーに同意します
                 </label>
               </div>
